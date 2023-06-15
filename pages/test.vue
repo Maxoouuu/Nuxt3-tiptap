e5eb182bed8ab864c72ca80eb5052aa92cadbe66
Maxime Coriton <maxime.coriton@gmail.com>
2023-06-15 14:42:16 +0200

    render br instead of empty <p></p>

---
+++ pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Exemple 1 sans br</p>
+    <span v-html="texte"></span>
+
+    <p>Exemple 2 avec br</p>
+    <span v-html="texte.replace(/<p><\/p>/g, '<br />')"></span>
+</template>
+
+<script setup>
+const texte = "<p>a</p><p>a</p><p></p><p>a</p>"
+</script>
